c70e0dc15e2012dfd4884d3d3c1487b0f120bcb5
SergioHs <shs_28@hotmail.com>
2025-06-12 21:44:24 -0300

    Feat: cria politicas de cors

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const UsuarioRoutes = require('./src/routes/UsuarioRoutes');
 const AuthRoutes = require('./src/routes/AuthRoutes');
 const RootRoutes = require('./src/routes/RootRoutes');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
